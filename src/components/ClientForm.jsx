22c0c73a6925e07de7634ea1ce063c3ac222263a
Nico Angelo  Garilva <nicogarilva@gmail.com>
2026-09-20 16:47:18 +0800

    feat: add client persistence and delete action UI

---
+++ src/components/ClientForm.jsx
@@ -0,0 +1,41 @@
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const ClientForm = forwardRef(function ClientForm({ setList }, ref) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => ({
+    open() {
+      dialog.current.showModal();
+    },
+  }));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const fd = new FormData(event.target);
+    const data = Object.fromEntries(fd.entries());
+    const newdata = { ...data, projects: 0, id: Date.now() };
+    setList((prevList) => [...prevList, newdata]);
+    dialog.current.close();
+    event.target.reset();
+  }
+
+  return (
+    <>
+      <dialog ref={dialog}>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" />
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" name="email" />
+          <button type="button" onClick={() => dialog.current.close()}>
+            Cancel
+          </button>
+          <button type="submit">Add Client</button>
+        </form>
+      </dialog>
+    </>
+  );
+});
+
+export default ClientForm;
--- src/pages/Clients.jsx
+++ src/pages/Clients.jsx
@@ -1,14 +1,32 @@
 import { Plus } from "lucide-react";
 import { Link } from "react-router-dom";
 import { clients } from "../data/clients";
+import { useEffect, useRef, useState } from "react";
+import ClientForm from "../components/ClientForm";
 
 export default function Clients() {
   const cardsStyle =
-    "flex flex-col  md:flex-row gap-5 justify-between border border-gray-200 rounded-xl p-6 text-sm text-gray-800 cursor-pointer hover:border-gray-300 hover:shadow-sm transition";
+    "flex justify-between border border-gray-200 rounded-xl p-6 text-sm text-gray-800 cursor-pointer hover:border-gray-300 hover:shadow-sm transition";
   const mutedText = "text-gray-400 my-auto";
   const nameStyle = "text-gray-900 font-semibold text-base";
   const primaryButt =
     "flex items-center gap-2 self-start px-4 py-2 rounded-lg text-white bg-blue-600 transition-colors hover:bg-blue-700";
+  const deleteButt =
+    "px-4 py-2 cursor-pointer self-center text-md rounded-lg text-red-600 border border-transparent hover:text-red-900 hover:bg-red-50 hover:border-red-200 transition-colors";
+  const clientForm = useRef();
+
+  const savedClients = JSON.parse(localStorage.getItem("clients"));
+  const [clientList, setClientList] = useState(savedClients || clients);
+
+  useEffect(() => {
+    localStorage.setItem("clients", JSON.stringify(clientList));
+  }, [clientList]);
+
+  function handleAddClient() {
+    clientForm.current.open();
+  }
+
+  function handleDeleteClient() {}
 
   return (
     <div className="max-w-5xl">
@@ -16,28 +34,33 @@ export default function Clients() {
         <h1 className="text-2xl font-semibold text-gray-900 mt-1 mb-2">
           Clients
         </h1>
-        <button className={primaryButt}>
+        <button className={primaryButt} onClick={handleAddClient}>
           <Plus className="size-4" /> Add Client
         </button>
       </div>
+
+      <ClientForm ref={clientForm} setList={setClientList} />
 
       <p className="text-sm text-gray-700 mb-10">
         Manage your clients and their projects.
       </p>
 
       <section className="flex gap-4 flex-col max-w-5xl">
-        {clients.map((client) => (
-          <Link to={`/clients/${client.id}`} key={client.id}>
-            <div className={cardsStyle}>
-              <div className="flex-1 min-w-0">
-                <p className={nameStyle}>{client.name}</p>
-                <p className={`${mutedText} wrap-break-word`}>{client.email}</p>
-              </div>
+        {clientList.map((client) => (
+          <div key={client.id} className={cardsStyle}>
+            <Link
+              to={`/clients/${client.id}`}
+              className="flex flex-col flex-1  gap-2 "
+            >
+              <p className={nameStyle}>{client.name}</p>
+              <p className={`${mutedText} wrap-break-word`}>{client.email}</p>
+
               <p className={`${mutedText} shrink-0`}>
                 {client.projects} {client.projects > 1 ? "Projects" : "Project"}
               </p>
-            </div>
-          </Link>
+            </Link>
+            <button className={deleteButt}>Delete</button>
+          </div>
         ))}
       </section>
     </div>
